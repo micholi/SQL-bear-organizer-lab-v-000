d8d2e0981fb79c413d6be0f8465f3feff97c8d1c
Michelle Olivieri <michelleo72@gmail.com>
2018-04-24 16:09:57 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,19 +7,19 @@ def selects_all_bears_names_and_orders_in_alphabetical_order
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
-  "SELECT name, age FROM bears WHERE alive = 1 ORDER BY age"
+  "SELECT name, age FROM bears WHERE alive = 1 ORDER BY age;"
 end
 
 def selects_oldest_bear_and_returns_name_and_age
-  "SELECT name, age FROM bears ORDER BY age DESC LIMIT 1"
+  "SELECT name, age FROM bears ORDER BY age DESC LIMIT 1;"
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  "SELECT name, age FROM bears ORDER BY age LIMIT 1"
+  "SELECT name, age FROM bears ORDER BY age LIMIT 1;"
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT"
+  "SELECT color, COUNT(color) FROM bears;"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
