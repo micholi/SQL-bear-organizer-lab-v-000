14786b4bd441eb6c250247b1d83b2ca326bff24d
Michelle Olivieri <michelleo72@gmail.com>
2018-04-24 16:31:03 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,9 +1,9 @@
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
-('Mr. Chocolate', 10, 'M', 'brown', 'friendly', 1),
-('Rowdy', 5, 'M', 'blue black', 'rowdy', 1),
-('Tabitha', 3, 'F', 'white', 'sweet', 1),
-('Sergeant Brown', 18, 'M', 'dark brown', 'bossy', 1),
-('Melissa', 12, 'F', 'black', 'gentle', 1),
-('Grinch', 7, 'M', 'green', 'mean', 1),
-('Wendy', 1, 'F', 'brown', 'playful', 1),
-(NULL, NULL, 'M', 'cinnamon', 'aggressive', 0);
+(1, 'Mr. Chocolate', 10, 'M', 'brown', 'friendly', 1),
+(2,'Rowdy', 5, 'M', 'blue black', 'rowdy', 1),
+(3, 'Tabitha', 3, 'F', 'white', 'sweet', 1),
+(4, 'Sergeant Brown', 18, 'M', 'dark brown', 'bossy', 1),
+(5, 'Melissa', 12, 'F', 'black', 'gentle', 1),
+(6, 'Grinch', 7, 'M', 'green', 'mean', 1),
+(7, 'Wendy', 1, 'F', 'brown', 'playful', 1),
+(8, NULL, NULL, 'M', 'cinnamon', 'aggressive', 0);
